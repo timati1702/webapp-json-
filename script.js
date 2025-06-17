0348b36e5c2de981eeea65dd7822c4a333b9edc3
Студент <student@NLA.LOCAL>
2025-06-17 15:35:39 +0300

    feat: change script

--- script.js
+++ script.js
@@ -3,14 +3,23 @@ document.addEventListener('DOMContentLoaded', function(){
     const body = document.body;
 
     function makeRequest() {
-        const link = randomElement(['https://jsonplaceholder.typicode.com/posts', 'https://jsonplaceholder.typicode.com/todos/1', 'https://jsonplaceholder.typicod.com/tdos/1']);
-        
+        const link = randomElement(Array('https://jsonplaceholder.typicode.com/posts', 'https://jsonplaceholder.typicode.com/todos/1', 'https://jsonplaceholder.typicod.com/tdos/1'));
+
         fetch(link)
             .then(response => {
                 if(!response.ok) {
-                    
-                }
-            });
+                    throw new Error('Network response is not ok' + response.statusText);
+                } 
+                return response.json();
+            })
+            .then(data => {
+                document.body.style.background = 'green';
+                console.log(data);
+            })
+            .catch(error => {
+                document.body.style.backgroundColor = 'red';
+                console.log('Ошибка:', error);
+            })
     };
 
     function randomElement(arr) {
